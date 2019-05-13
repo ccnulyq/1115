beb44eb9200e33ab29675659d1aacf1475a45a58
laoluan <ccnulyq@163.com>
2019-05-13 19:58:22 +0800

    修改TestGit类，并第二次提交

--- MyGit/src/cn/gzsxt/git/test/TestGit.java
+++ MyGit/src/cn/gzsxt/git/test/TestGit.java
@@ -5,4 +5,8 @@ public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("hello,git");
 	}
+	
+	public void say(){
+		System.out.println("this is the first git project!");
+	}
 }
